3b101a82cff2f4b72c9dea0cdc766fb85879ac6f
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-09 00:10:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 //complete this code
 class Person {
-	(name, age)
+	// (name, age)
+	 constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  set age(value) {
+    this._age = value;
+  }
 }
 
 class Student extends Person {
-	study() {
-		console.log(`${Person.name} is studying`);
-	}
+	 study() {
+    console.log(`${this._name} is studying.`);
+  }
 }
 
 class Teacher extends Person {
 	teach() {
-		console.log(`${Person.name} is teaching`);
-	}
+    console.log(`${this._name} is teaching.`);
+  }
 }
 
 // Do not change the code below this line
